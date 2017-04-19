5856b391ade10146a91a1ce383e747cb5875e5f8
Rhys <rimmington@gmail.com>
2017-04-19 17:53:50 +1000

    FIX: Proper example module with good suPath

--- module.nix
+++ module.nix
@@ -1,12 +1,40 @@
-{pkgs, ...}:
+{config, lib, pkgs, ...}:
+
+with lib;
 
 let
-  fragile = pkgs.callPackage ./. {};
+  cfg = config.services.fragile;
+  fragile = pkgs.callPackage ./. { suPath = "${config.security.wrapperDir}/su"; };
 in {
-  environment.systemPackages = [ fragile ];
-  security.sudo.extraConfig = ''
-    Cmnd_Alias FRAGILE = ${fragile}/bin/fragile
-    Defaults!FRAGILE env_keep += NIX_PATH
-    %nixbld ALL = NOPASSWD: FRAGILE
-  '';
+  options = {
+    services.fragile.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to enable fragile, placing it in the system packages.
+        This may have security implications.
+      '';
+    };
+
+    services.fragile.permitNixbldSudo = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        Whether to permit passwordless sudo access to fragile for users in the
+        nixbld group.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ fragile ];
+
+    security.sudo.extraConfig = mkIf cfg.permitNixbldSudo ''
+
+      # services.fragile.permitNixbldSudo
+      Cmnd_Alias FRAGILE = ${fragile}/bin/fragile
+      Defaults!FRAGILE env_keep += NIX_PATH
+      %nixbld ALL = NOPASSWD: FRAGILE
+    '';
+  };
 }
